25f29898703082ca8b6a19707091d2a99d3207df
sunny <sunny@CL2017B>
2017-01-01 12:42:10 -0500

    This one doing a dropdown of houses to show a dropdown

--- HogWarsh1/HogWarsh1/Controllers/StudentController.cs
+++ HogWarsh1/HogWarsh1/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HogWarsh1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,15 +12,30 @@ namespace HogWarsh1.Controllers
         [HttpGet]
         public ActionResult Enroll()
         {
-            var vm = new Models.Student() { Name = "a" };
+            var vm = new StudentEnrollViewModel() {
+                Student = new Student(),
+                Houses = InMemoryDatabase.GetAllHouses()
+            };
             return View(vm);
         }
 
         [HttpPost]
-        public ActionResult Enroll(Models.Student studentvm)
+        public ActionResult Enroll(StudentEnrollViewModel studentvm)
         {
-            studentvm.Name = "This modification is ignored due to how EditorFor works";
+            // TODO: put in database and send user somewhere
+            
+            // when demoing this, show the generated HTML from the previous commit vs this commit, how the input fields
+            // change as you change view models
+
             return View(studentvm);
+        }
+
+        public class StudentEnrollViewModel
+        {
+            // could move this out to Models.
+
+            public Student Student { get; set; }
+            public List<House> Houses { get; set; }
         }
 
     }
