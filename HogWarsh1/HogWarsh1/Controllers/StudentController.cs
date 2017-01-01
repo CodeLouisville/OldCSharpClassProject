05f9b57636d56de8494ebc2bd7700c8489595f02
sunny <sunny@CL2017B>
2017-01-01 12:32:47 -0500

    Initial student data entry screen that posts back

--- HogWarsh1/HogWarsh1/Controllers/StudentController.cs
+++ HogWarsh1/HogWarsh1/Controllers/StudentController.cs
@@ -8,10 +8,19 @@ namespace HogWarsh1.Controllers
 {
     public class StudentController : Controller
     {
+        [HttpGet]
         public ActionResult Enroll()
         {
-            var vm = new Models.Student(); 
+            var vm = new Models.Student() { Name = "a" };
             return View(vm);
         }
+
+        [HttpPost]
+        public ActionResult Enroll(Models.Student studentvm)
+        {
+            studentvm.Name = "This modification is ignored due to how EditorFor works";
+            return View(studentvm);
+        }
+
     }
 }
