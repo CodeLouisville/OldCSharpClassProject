c3feef3b5a835b3ed2420f1761d3c7b30bcdf188
sunny <sunny@CL2017B>
2017-01-01 12:19:02 -0500

    Wired up a Enroll button to go to the student controller

---
+++ HogWarsh1/HogWarsh1/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HogWarsh1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ActionResult Enroll()
+        {
+            var vm = new Models.Student(); 
+            return View(vm);
+        }
+    }
+}
