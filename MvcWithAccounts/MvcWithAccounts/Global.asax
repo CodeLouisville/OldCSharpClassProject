b9cc1ebb14fdc838a2da40fe9b057d8ce16ceda1
Sunny Gulati <sunnywiz@gmail.com>
2017-01-22 17:35:10 -0500

    Moved .gitignore up one more level; added MvcWithAccounts; end of proposed week 2

---
+++ MvcWithAccounts/MvcWithAccounts/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcWithAccounts.MvcApplication" Language="C#" %>
---
+++ MvcWithAccounts/MvcWithAccounts/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcWithAccounts.Startup))]
+namespace MvcWithAccounts
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
