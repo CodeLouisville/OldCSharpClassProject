7853238f551d1587173159bbfb7c4ab12dc89cbf
Sunny Gulati <sunnywiz@gmail.com>
2017-01-22 15:06:50 -0500

    new MVC project and forgot to commit earlier, but added a little view with two fields and a model

---
+++ HelloWorldMVC/HelloWorldMVC/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorldMVC.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
