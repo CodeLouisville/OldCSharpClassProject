5dc005fe35a9255d627c2201aec09636e5bd8ffd
Sunny Gulati <sunnywiz@gmail.com>
2017-01-23 18:31:30 -0500

    Hello person really bad code but that's what we did in 31 minutes.

---
+++ MVCHelloWorld/MVCHelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using MVCHelloWorld.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var person = new Person()
+            {
+                FirstName = "bob",
+                LastName = "uncle"
+            };
+            return View(person);
+        }
+
+        public ActionResult SayHello(Person person)
+        {
+            person.FirstName = person.FirstName.ToUpperInvariant();
+            person.LastName = person.LastName.ToUpperInvariant();
+            return View(person);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
