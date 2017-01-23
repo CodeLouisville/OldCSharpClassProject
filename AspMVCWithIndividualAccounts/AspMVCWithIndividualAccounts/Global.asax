b40cb44614cea20272a4706303f9faf7049b8945
Sunny Gulati <sunnywiz@gmail.com>
2017-01-23 18:37:36 -0500

    Added asp.net app with identity stuff

---
+++ AspMVCWithIndividualAccounts/AspMVCWithIndividualAccounts/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspMVCWithIndividualAccounts.MvcApplication" Language="C#" %>
---
+++ AspMVCWithIndividualAccounts/AspMVCWithIndividualAccounts/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspMVCWithIndividualAccounts.Startup))]
+namespace AspMVCWithIndividualAccounts
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
