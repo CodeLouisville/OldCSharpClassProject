46349ad47bcb9c879279abef8212923ada6766f8
Sunny Gulati <sunnywiz@gmail.com>
2017-02-13 18:36:32 -0500

    Getting data back in a model via POST

--- HogWarsh/HogWarsh/Controllers/StudentController.cs
+++ HogWarsh/HogWarsh/Controllers/StudentController.cs
@@ -1,12 +1,21 @@
-﻿using System.Web.Mvc;
+﻿using HogWarsh.Models;
+using System.Web.Mvc;
 
 namespace HogWarsh.Controllers
 {
     public class StudentController : Controller
     {
+        [HttpGet]
         public ActionResult Enroll()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult ActuallyEnroll(Student student)
+        {
+            return View();
+        }
+
     }
 }
---
+++ HogWarsh/HogWarsh/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HogWarsh.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+    }
+}
