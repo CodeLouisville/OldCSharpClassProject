02d2b33874b5cff18d307ff375ba6b42910ac4eb
Sunny Gulati <sunnywiz@gmail.com>
2017-02-13 18:22:45 -0500

    I made a view!  it doesn't do much.

---
+++ HogWarsh/HogWarsh/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace HogWarsh.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ActionResult Enroll()
+        {
+            return View();
+        }
+    }
+}
