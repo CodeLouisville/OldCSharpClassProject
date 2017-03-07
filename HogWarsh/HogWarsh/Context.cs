4c9e749e9286511ed68d4ec345b5966ba241270c
Sunny Gulati <sunnywiz@gmail.com>
2017-03-06 19:01:39 -0500

    This is with seed data.

--- HogWarsh/HogWarsh/Context.cs
+++ HogWarsh/HogWarsh/Context.cs
@@ -7,9 +7,30 @@ namespace HogWarsh
     {
         public Context() :base("HogWarshConnectionString")
         {
-
+            Database.SetInitializer(new MyInitializer());
         }
         public DbSet<Student> Students { get; set; }
         public DbSet<House> Houses { get; set; }
+    }
+    public class MyInitializer : 
+        DropCreateDatabaseIfModelChanges<Context>
+        //DropCreateDatabaseAlways<Context>
+    {
+        protected override void Seed(Context context)
+        {
+            context.Houses.Add(new House()
+            {
+                Name = "Furrby"
+            });
+            context.Houses.Add(new House()
+            {
+                Name = "NomNom"
+            });
+            context.Houses.Add(new House()
+            {
+                Name = "Wagabond"
+            });
+            base.Seed(context);
+        }
     }
 }
