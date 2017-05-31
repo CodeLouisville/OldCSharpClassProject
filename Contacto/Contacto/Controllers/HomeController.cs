badc847988f2e21807632a282852aaee7e476e62
Matt Durham <mattdurham@ppog.org>
2017-05-30 22:57:26 -0400

    Added contacto from week 3

---
+++ Contacto/Contacto/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using Contacto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Contacto.Controllers
+{
+    public class HomeController : Controller
+    {
+        static ContactRepository _contactRepository = new ContactRepository();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult CreateContact()
+        {
+            return View("AddContact");
+        }
+
+        public ActionResult AddContact(Contact contact)
+        {
+            _contactRepository.AddContact(contact);
+            return View("SingleContact",contact);
+        }
+
+
+        public ActionResult ViewContacts()
+        {
+            return View("ContactList", _contactRepository.Contacts);
+        }
+    }
+
+    public class ContactRepository
+    {
+        private List<Contact> _contacts = new List<Contact>();
+
+        public void AddContact(Contact contact)
+        {
+            _contacts.Add(contact);
+        }
+
+        public List<Contact> Contacts
+        {
+            get
+            {
+                return _contacts;
+            }
+        }
+    }
+}
---
+++ Contacto/Contacto/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Contacto.Models
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
