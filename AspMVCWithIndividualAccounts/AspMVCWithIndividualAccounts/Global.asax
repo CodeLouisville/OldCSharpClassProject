715663230fc78ce3f09f35856d22bf9aed258c6d
Matt Durham <mattdurham@ppog.org>
2017-06-04 11:10:11 -0400

    REmoved unused projects

--- AspMVCWithIndividualAccounts/AspMVCWithIndividualAccounts/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspMVCWithIndividualAccounts.MvcApplication" Language="C#" %>
--- HogWarsh/Hogwarsh.Data/House.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Student]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-	[Name] Varchar(30) NOT NULL, 
-	[Species] varchar(6) NOT NULL,
-	[HouseId] INT NULL
-)
